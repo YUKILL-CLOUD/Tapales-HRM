ec5f796753635325f1b69bae57ba9fd5fbb14ae8
paul bernard <bartolopaul11@gmail.com>
2024-06-24 00:49:52 +0800

    added features

--- components/Frontend/hero.tsx
+++ components/Frontend/hero.tsx
@@ -1,47 +1,50 @@
-import React from 'react'
+import React from 'react';
 
 export default function HeroSection() {
   return (
-                <section>
-                    <div className="max-w-screen-xl mx-auto px-4 py-28 gap-12 text-gray-600 overflow-hidden md:px-8 md:flex min-h-screen flex items-center justify-center">
-                        <div className='flex-none space-y-5 max-w-xl'>
-                            <a href="javascript:void(0)" className='inline-flex gap-x-6 items-center rounded-full p-1 pr-6 border text-sm font-medium duration-150 hover:bg-white'>
-                                <span className='inline-block rounded-full px-3 py-1 bg-indigo-600 text-white'>
-                                    News
-                                </span>
-                                <p className='flex items-center'>
-                                    Read the launch post from here
-                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5">
-                                        <path fillRule="evenodd" d="M7.21 14.77a.75.75 0 01.02-1.06L11.168 10 7.23 6.29a.75.75 0 111.04-1.08l4.5 4.25a.75.75 0 010 1.08l-4.5 4.25a.75.75 0 01-1.06-.02z" clipRule="evenodd" />
-                                    </svg>
-                                </p>
-                            </a>
-                            <h1 className="text-4xl text-gray-800 font-extrabold sm:text-5xl">
-                                Build your SaaS exactly how you want
-                            </h1>
-                            <p>
-                                Sed ut perspiciatis unde omnis iste natus voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae.
-                            </p>
-                            <div className='flex items-center gap-x-3 sm:text-sm'>
-                                <a href="javascript:void(0)" className="flex items-center justify-center gap-x-1 py-2 px-4 text-white font-medium bg-gray-800 duration-150 hover:bg-gray-700 active:bg-gray-900 rounded-full md:inline-flex">
-                                    Get started
-                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5">
-                                        <path fillRule="evenodd" d="M7.21 14.77a.75.75 0 01.02-1.06L11.168 10 7.23 6.29a.75.75 0 111.04-1.08l4.5 4.25a.75.75 0 010 1.08l-4.5 4.25a.75.75 0 01-1.06-.02z" clipRule="evenodd" />
-                                    </svg>
-                                </a>
-                                <a href="javascript:void(0)" className="flex items-center justify-center gap-x-1 py-2 px-4 text-gray-700 hover:text-gray-900 font-medium duration-150 md:inline-flex">
-                                    Contact sales
-                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5">
-                                        <path fillRule="evenodd" d="M7.21 14.77a.75.75 0 01.02-1.06L11.168 10 7.23 6.29a.75.75 0 111.04-1.08l4.5 4.25a.75.75 0 010 1.08l-4.5 4.25a.75.75 0 01-1.06-.02z" clipRule="evenodd" />
-                                    </svg>
-                                </a>
-                            </div>
-                        </div>
-                        <div className='flex-1 hidden md:block'>
-                            {/* Replace with your image */}
-                            <img src="https://raw.githubusercontent.com/sidiDev/remote-assets/c86a7ae02ac188442548f510b5393c04140515d7/undraw_progressive_app_m-9-ms_oftfv5.svg" className="max-w-xl" />
-                        </div>
-                    </div>
-                </section>
-  )
+    <section>
+      <div className="max-w-screen-xl mx-auto px-4 py-20 gap-12 text-gray-600 overflow-hidden md:px-8 md:flex md:min-h-screen md:items-center md:justify-center">
+        <div className="flex-none space-y-5 max-w-xl">
+        
+          <h1 className="text-4xl text-mainColor font-extrabold sm:text-5xl">
+          Streamline Your Pet's Care
+          </h1>
+          <p>
+          Effortlessly 
+          <span className=' hover:text-mainHover mr-2 ml-2'>manage</span>
+          your pet's appointments, health records, and more with our intuitive clinic management system. Stay organized, stay informed, and give your pet the care they deserve.
+          </p>
+          <div className="flex items-center gap-x-3 sm:text-sm">
+            <a
+              href="javascript:void(0)"
+              className="flex items-center justify-center gap-x-1 py-2 px-4 text-white font-medium bg-mainColor duration-150 hover:bg-mainHover active:bg-gray-900 rounded-full md:inline-flex"
+            >
+              Get started
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+                className="w-5 h-5"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M7.21 14.77a.75.75 0 01.02-1.06L11.168 10 7.23 6.29a.75.75 0 111.04-1.08l4.5 4.25a.75.75 0 010 1.08l-4.5 4.25a.75.75 0 01-1.06-.02z"
+                  clipRule="evenodd"
+                />
+              </svg>
+            </a>
+            
+          </div>
+        </div>
+        <div className="flex-1 hidden md:block">
+          {/* Replace with your image */}
+          <img
+            src="pngegg.png"
+            className="max-w-xl"
+            alt="Illustration"
+          />
+        </div>
+      </div>
+    </section>
+  );
 }
